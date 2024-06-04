1f60ae732570b5b3a0d62c7c43cc5b4324202903
ZIIM <johntgause12@gmail.com>
2024-06-04 13:30:38 -0700

    refactor

--- DB_Vender/vendersqlitedb.js
+++ DB_Vender/vendersqlitedb.js
@@ -9,8 +9,13 @@ const db = new sqlite3.Database(':memory:');
 
 db.serialize(() => {
     db.run('DROP TABLE IF EXISTS vender');
+    db.run('DROP TABLE IF EXISTS products');
     db.run('CREATE TABLE vender (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, location TEXT, storeID INTEGER)', () => {
-        db.run('INSERT INTO vender (name, location, storeID) VALUES (?, ?, ?)', ['Brocks Bananas', 'Seattle, WA', '123456789']);
+        db.run('INSERT INTO vender (name, location, storeID) VALUES (?, ?, ?)', ['Brocks Bananas', 'Seattle, WA', 123456789]);
+    });
+
+    db.run('CREATE TABLE products (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, amount INTEGER, price REAL, venderID INTEGER, FOREIGN KEY(venderID) REFERENCES vender(id))', () => {
+        db.run('INSERT INTO products (name, amount, price, venderID) VALUES (?, ?, ?, ?)', ['Bananas', 12, 2.99, 1]);
     });
 });
 
@@ -28,21 +33,47 @@ app.post('/vender', (req, res) => {
 });
 
 app.get('/vender', (req, res) => {
-    db.all('SELECT * FROM vender', [], (err, rows) => {
+    db.all('SELECT * FROM vender', [], (err, vendors) => {
         if (err) {
             return res.status(500).json({ error: err.message });
         }
-        res.json({ vender: rows });
+
+        const vendorIds = vendors.map(v => v.id);
+        db.all('SELECT * FROM products WHERE venderID IN (' + vendorIds.join(',') + ')', [], (err, products) => {
+            if (err) {
+                return res.status(500).json({ error: err.message });
+            }
+
+            const vendorsWithProducts = vendors.map(vendor => {
+                return {
+                    ...vendor,
+                    products: products.filter(product => product.venderID === vendor.id)
+                };
+            });
+
+            res.json({ vendors: vendorsWithProducts });
+        });
     });
 });
 
 app.get('/vender/:id', (req, res) => {
     const { id } = req.params;
-    db.get('SELECT * FROM vender WHERE id = ?', [id], (err, row) => {
+    db.get('SELECT * FROM vender WHERE id = ?', [id], (err, vendor) => {
         if (err) {
             return res.status(500).json({ error: err.message });
         }
-        res.json({ vender: row });
+        if (!vendor) {
+            return res.status(404).json({ error: "Vendor not found" });
+        }
+
+        db.all('SELECT * FROM products WHERE venderID = ?', [id], (err, products) => {
+            if (err) {
+                return res.status(500).json({ error: err.message });
+            }
+
+            vendor.products = products;
+            res.json({ vendor });
+        });
     });
 });
 
@@ -68,3 +99,4 @@ app.delete('/vender/:id', (req, res) => {
 });
 
 module.exports = app;
+
